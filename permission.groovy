31e099444815b76a6a1a804a6be827598037193f
rajughosh <raju.ghosh@fieldnation.com>
2017-05-30 17:12:25 +0600

    added all file

---
+++ permission.groovy
@@ -0,0 +1,28 @@
+import jenkins.model.*
+import hudson.security.*
+import groovy.json.JsonSlurper
+def inputJson = new File("users.json")
+def userlist = new JsonSlurper().parse(inputJson)
+def instance = Jenkins.getInstance()
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+def strategy = instance.getAuthorizationStrategy();
+
+userlist.admin.each { admingroup ->
+    hudsonRealm.createAccount("${admingroup}","${admingroup}")
+    strategy.add(Jenkins.ADMINISTER, "${admingroup}")
+}
+
+userlist.developer.each { developergroup ->
+hudsonRealm.createAccount("${developergroup}","${developergroup}")
+      //  Job Build and view Permissions
+    strategy.add(hudson.model.Hudson.READ,"${developergroup}")
+    strategy.add(hudson.model.Item.BUILD,"${developergroup}")
+    strategy.add(hudson.model.Item.CANCEL,"${developergroup}")
+    strategy.add(hudson.model.Item.DISCOVER,"${developergroup}")
+    strategy.add(hudson.model.Item.READ,"${developergroup}")
+    strategy.add(hudson.model.Item.WORKSPACE,"${developergroup}")
+}
+instance.setAuthorizationStrategy(strategy)
+instance.setSecurityRealm(hudsonRealm)
+instance.save()
+
